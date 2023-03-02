1185cde41f15ad20de84ef3c92cf802d6b2f9edd
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-03-02 22:08:36 +0300

    d03v4

---
+++ postgre-tp/day03v4.sql
@@ -0,0 +1,28 @@
+
+-- AGGREGATE METHODS
+
+select * from employees
+select * from workplaces
+
+select max(salary) from employees
+select max(salary) as max_salary from employees -- temp data name
+
+select min(salary) from employees
+
+select sum(salary) from employees
+
+select avg(salary) from employees -- average 104166.666666666667
+select round(avg(salary),2) from employees -- average 104166.67
+
+select count(salary) from employees  -- 6
+select count(*) from employees  -- counts rows (include null)
+
+-- AGGREGATE METHODS SUBQUERY
+select wpname,(select count(city) from employees where wpname=workplace) from workplaces;
+
+
+
+
+
+
+
