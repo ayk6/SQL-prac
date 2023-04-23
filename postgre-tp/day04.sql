2cce1332378807687b6a9ac0161c15e97fd44d23
Ahmet Y. KARACA <av.ahmetyasinkaraca@gmail.com>
2023-04-23 22:46:42 +0300

    d4l

--- postgre-tp/day04.sql
+++ postgre-tp/day04.sql
@@ -1,6 +1,8 @@
 -- aliases
 -- change only for output
 
+select * from employees
+
 select empid as id, empname || city as nameandcity from employees  
 -- or 
 select empid as id, concat (empname,city) as nameandcity from employees 
@@ -12,4 +14,7 @@ select * from students where stuid is null
 select * from students order by stuaverage
 select * from students order by stuaverage desc -- for reverse
 select * from students order by length (stuname) -- data length
+
+-- group by clause
+select city,sum(salary) from employees group by city
 
